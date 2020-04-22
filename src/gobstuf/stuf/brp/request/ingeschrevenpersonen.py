9bb0cb72d2a00cc3140ad248b9c7fb39ad69dd7f
vegter <amsjurien@gmail.com>
2020-04-22 15:24:59 +0200

    Update ingeschreven personen requests and responses
    Update the template to retrieve persoonsgegevens
    Update the request to validate the parameters (bsn)
    Update the response to filter the result for overleden personen

--- src/gobstuf/stuf/brp/request/ingeschrevenpersonen.py
+++ src/gobstuf/stuf/brp/request/ingeschrevenpersonen.py
@@ -6,6 +6,28 @@ class IngeschrevenpersonenStufRequest(StufRequest):
     content_root_elm = 'soapenv:Body BG:npsLv01'
     soap_action = 'http://www.egem.nl/StUF/sector/bg/0310/npsLv01Integraal'
 
+    BSN_LENGTH = 9
+
     replace_paths = {
         'bsn': 'BG:gelijk BG:inp.bsn'
     }
+
+    def validate(self, args):
+        # print("VALIDATE BSN")
+        bsn = args['bsn']
+        if len(bsn) != self.BSN_LENGTH:
+            name = "burgerservicenummer"
+            if len(bsn) < self.BSN_LENGTH:
+                invalid_param = {
+                    "code": "minLength",
+                    "reason": f"Waarde is korter dan minimale lengte {self.BSN_LENGTH}."
+                }
+            else:
+                invalid_param = {
+                    "code": "maxLength",
+                    "reason": f"Waarde is langer dan maximale lengte {self.BSN_LENGTH}."
+                }
+            invalid_param['name'] = name
+            return self.params_errors([name], [invalid_param])
+
+        return super().validate(args)
